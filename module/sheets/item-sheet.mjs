33a08132250fe2313977719cf04f4b509dab1e71
Quemares <felimenares@hotmail.com>
2025-04-03 22:31:34 -0300

    feat: weapon items can now be created

--- module/sheets/item-sheet.mjs
+++ module/sheets/item-sheet.mjs
@@ -2,4 +2,12 @@ export class NexusItemSheet extends ItemSheet {
     get template() {
         return `systems/nexusblank/templates/sheets/${this.item.type}-sheet.html`;
     }
+
+    getData(){
+        const data = super.getData();
+
+        data.config = CONFIG.nexus;
+
+        return data;
+    }
 }
--- nexus.js
+++ nexus.js
@@ -1,7 +1,10 @@
-import { NexusItemSheet } from "./sheets/item-sheet.mjs";
+import { nexus } from "./module/config.js";
+import { NexusItemSheet } from "./module/sheets/item-sheet.mjs";
 
 Hooks.once('init', function(){
     console.log("nexus | Initialising Nexus System");
+
+    CONFIG.nexus = nexus;
 
     Items.unregisterSheet('core', ItemSheet);
     Items.registerSheet('nexus', NexusItemSheet, {
